03db5a33a946b26c26c1406a7cd52c05fd5061a1
Shriram Sharma <ssharma5@walmartlabs.com>
2016-09-01 08:21:48 -0700

    Added test for NewLRU constructor

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -6,5 +6,22 @@ func TestNewLRU(t *testing.T) {
 	if _, err := NewLRU(0); err == nil {
 		t.Errorf("Expected an invalid size error")
 	}
+	size := 128
+	cache, err := NewLRU(size)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if cache == nil {
+		t.Errorf("Error in instantiating cache")
+	}
+	if cache.pageList == nil {
+		t.Errorf("pageList is nil")
+	}
+	if cache.lookupTable == nil {
+		t.Errorf("lookupTable is nil")
+	}
+	if cache.size != size {
+		t.Errorf("Incorrect cache size")
+	}
 
 }
